51af7ddd155ad19c8287a6d5fabc41dfa6e620bc
Dawn Pattison <pattisdr@users.noreply.github.com>
2024-06-16 14:41:49 -0500

    Remove python 3.12 support

--- noxfile.py
+++ noxfile.py
@@ -4,7 +4,7 @@ nox.options.sessions = []
 nox.options.reuse_existing_virtualenvs = True
 
 # These should match what is in the `pr_checks.yml` file for CI runs
-TESTED_PYTHON_VERSIONS = ["3.9", "3.10", "3.11", "3.12"]
+TESTED_PYTHON_VERSIONS = ["3.9", "3.10", "3.11"]
 TESTED_PYDANTIC_VERSIONS = ["2.2.1", "2.3.0", "2.4.2", "2.7.1"]
 TESTED_PYYAML_VERSIONS = ["5.5.1", "6.0.1"]
 
